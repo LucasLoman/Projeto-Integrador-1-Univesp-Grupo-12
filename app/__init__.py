8a8d7c4300f478452683fd3a440c356499ca9130
LucasLoman <lucasloman@hotmail.com>
2024-04-14 00:50:38 -0300

    Arrumando
    Arrumando cagadas

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-# ".\vemv\Scripts\activate" Para ligar o ambiente virtual
+# ".\venv\Scripts\activate" Para ligar o ambiente virtual
 from flask import Flask
 app= Flask(__name__)
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,15 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Olá"
+@app.route('/pagina_inicial')
+def pagina_inicial():
+    return render_template('HomePage.html')
+
+@app.route('/formulario')
+def formulario():
+    return render_template('Forms.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('Contato.html')
 
