1691f665965421c756bd814baedf8c9913a11188
vitor <107767584+vitorfloriano@users.noreply.github.com>
2024-04-21 17:05:20 -0300

    Adiciona a configuração do MariaDB
    Adiciona a configuração necessária para a conexão com o MariaDB.

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
 
 class Config:
+    # ...placeholder para outras configurações
+    # CONFIG_1 =
+    # CONFIG_2 =
+
+    # !!! Ainda será preciso configurar a conexão com mariadb
+    SQLALCHEMY_DATABASE_URI = 'mariadb://flask:flask@localhost/pi_1_flask'
     
