f218e3138716f8758693785720d47b56a12a7306
vitor <107767584+vitorfloriano@users.noreply.github.com>
2024-04-21 17:14:24 -0300

    Cria o primeiro model do app
    Os models são classes que o SQLAlchemy ORM utiliza para modelar o banco de dados.
    
    Foi criada uma classe simples de Candidato para teste.

---
+++ app/models.py
@@ -0,0 +1,16 @@
+import sqlalchemy as sqlal
+import sqlalchemy.orm as sqlorm
+from app import db
+
+class Candidato(db.Model):
+    id: sqlorm.Mapped[int] = sqlorm.mapped_column(primary_key=True)
+    candidato: sqlorm.Mapped[str] = sqlorm.mapped_column(sqlal.String(64), index=True, 
+                                                         unique=True)
+    email: sqlorm.Mapped[str] = sqlorm.mapped_column(sqlal.String(120), index=True,
+                                                     unique=True)
+    
+    # preencher com mais colunas, atributos, campos...
+
+    def __repr__(self):
+        return '<Candidato {}>'.format(self.candidato)
+    
