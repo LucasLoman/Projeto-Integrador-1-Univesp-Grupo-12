713bd62f9aad043880ae4b0e15d672b9b90fe1f8
vfcdebianprefs <vfcdebianprefs>
2024-04-25 16:52:50 -0300

    Arrumando a casa

--- app/models.py
+++ app/models.py
@@ -4,14 +4,11 @@ from app import db
 
 class Candidato(db.Model):
     id: sqlorm.Mapped[int] = sqlorm.mapped_column(primary_key=True)
-    nome: sqlorm.Mapped[str] = sqlorm.mapped_column(sqlal.String(64), index=True, 
-                                                    unique=True)
-    nasc: sqlorm.Mapped[int] = sqlorm.mapped_column(sqlal.Integer(), index=False, 
-                                                    unique=False)
-    email: sqlorm.Mapped[str] = sqlorm.mapped_column(sqlal.String(120), index=True,
-                                                    unique=True)
-    contato: sqlorm.Mapped[int] = sqlorm.mapped_column(sqlal.Integer(), index=False,
-                                                    unique=True)
+    nome: sqlorm.Mapped[str] = sqlorm.mapped_column(sqlal.String(64), index=True, unique=True)
+    email: sqlorm.Mapped[str] = sqlorm.mapped_column(sqlal.String(120), index=True, unique=True)
+    contato: sqlorm.Mapped[int] = sqlorm.mapped_column(sqlal.Integer(), index=False, unique=True)
+    
+
     
     # preencher com mais colunas, atributos, campos...
 
